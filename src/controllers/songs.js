fe38ba74f9e23b86829b9a7a2e82ec7c620a3204
Sofia <sofia.dionisio94@gmail.com>
2020-05-20 14:54:35 +0100

    Used include in listSongsByAlbum in order to get a more complete document, with artist and album info related to the song

--- src/controllers/songs.js
+++ src/controllers/songs.js
@@ -29,8 +29,9 @@ exports.listSongsByAlbum = (req,res) => {
     if(!album) {
       return res.status(404).json({ error: 'The album could not be found.' });
     }else{
-      Song.findAll({ include: [{ model: Album, as: 'album', where: {id: albumId} }] })
-      .then(songs => {return res.status(200).json(songs)});
+      Song.findAll({ 
+        include: [{ model: Album, as: 'album', where: {id: albumId} }, { model: Artist, as: 'artist', where: {id: album.artistId} }] })
+        .then(songs => {return res.status(200).json(songs)});
     };
   })
 };
